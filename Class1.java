726467a5e53e11e99b4e572574b69868a76d6b11
Harshal <harshaldeore05@gmail.com>
2023-02-02 13:47:54 +0530

    First commit

---
+++ Class1.java
@@ -0,0 +1,17 @@
+public class Class1{
+    public static void main(String[] args){
+     int[] a1 = {1,2,3,4,5};
+     int ans = 50;
+     System.out.println(ans+"found at"+search(a1,ans));
+    }
+
+    public static int search(int[] arr, int ans) {
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==ans){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
